247d34979d4d7bc71ecb8f6389134ff3c29f477e
MalekeHan <Malekehanson@gmail.com>
2023-03-27 17:33:31 -0700

    Adding some markdown capability

--- backend/controllers/openaiController.js
+++ backend/controllers/openaiController.js
@@ -28,27 +28,33 @@ exports.completionsController = async (req, res) => {
 };
 
 exports.chatCompletionsController = async (req, res) => {
-  try {
-    const inputMessages = req.body.messages;
-
-    const response = await axios.post(
-      'https://api.openai.com/v1/chat/completions',
-      {
-        model: 'gpt-3.5-turbo',
-        messages: inputMessages,
-        temperature: 0.8,
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    try {
+      const inputMessages = req.body.messages;
+  
+      const response = await axios.post(
+        'https://api.openai.com/v1/chat/completions',
+        {
+          model: 'gpt-3.5-turbo',
+          messages: inputMessages,
         },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          },
+        }
+      );
+  
+      const completion = response.data.choices[0].message.content;
+  
+      // Check if the completion contains a code snippet
+      if (completion.includes('```')) {
+        const formattedCompletion = `\`\`\`md\n${completion}\n\`\`\``;
+        res.status(200).json({ completion: formattedCompletion });
+      } else {
+        res.status(200).json({ completion });
       }
-    );
-
-    const completion = response.data.choices[0].message;
-    res.status(200).json({ completion });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
